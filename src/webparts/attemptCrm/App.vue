6dfb11073f69d93631a832d69dc098c7a85ef6cc
Scheevel <scheevel@gmail.com>
2020-03-27 16:20:35 -0500

    working ...app.vue coverted
    function are stubs in app.vue

--- src/webparts/attemptCrm/App.vue
+++ src/webparts/attemptCrm/App.vue
@@ -40,13 +40,126 @@
 </template>
 <script>
 import SpFxComponent from './components/AttemptCrm.vue';
+import auth from "./utils/auth";
+import { mapState } from "vuex";
 export default {
-  name: 'App',
   components: {
     SpFxComponent,
   },
-  data: () => ({
-    //
-  }),
+    data: () => {return {
+        dialog: false,
+        mini: false,
+        dialogText: "",
+        dialogTitle: "",
+        loggedIn: auth.loggedIn(),
+        isRootComponent: true,
+        // clipped: false,
+        drawer: window.innerWidth > 960,
+        fixed: false,
+        items: [
+          {
+            icon: "dashboard",
+            title: "Dashboard",
+            vertical: "Dashboard",
+            link: "dashboard"
+          },
+          {
+            icon: "shopping_cart",
+            title: "Orders",
+            vertical: "Order",
+            link: "orders"
+          },
+          {
+            icon: "perm_identity",
+            title: "Customers",
+            vertical: "Customer",
+            link: "customers"
+          },
+          {
+            icon: "bubble_chart",
+            title: "Products",
+            vertical: "Product",
+            link: "products"
+          },
+          {
+            icon: "thumbs_up_down",
+            title: "About",
+            vertical: "About",
+            link: "about"
+          }
+        ],
+        userMenus: [
+          {
+            icon: "bubble_chart",
+            title: "Logout",
+            link: "/logout"
+          },
+          {
+            icon: "bubble_chart",
+            title: "Change Password",
+            link: "/changepassword"
+          }
+        ],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        menuItem: "Orders"
+      };
+    },
+    created () {
+/*
+      auth.onChange = loggedIn => {
+        console.log("loggedIn", loggedIn);
+        this.loggedIn = loggedIn;
+      };
+      //  [App.vue specific] When App.vue is first loaded start the progress bar
+      this.$Progress.start();
+      //  hook the progress bar to start before we move router-view
+      this.$router.beforeEach((to, from, next) => {
+        //  does the page we want to go to have a meta.progress object
+        if (to.meta.progress !== undefined) {
+          let meta = to.meta.progress;
+          // parse meta tags
+          this.$Progress.parseMeta(meta);
+        }
+        //  start the progress bar
+        this.$Progress.start();
+        //  continue to next page
+        next();
+      });
+      //  hook the progress bar to finish after we've finished moving router-view
+      this.$router.afterEach((to, from) => {
+        if (to.name !== "ErrorPage") {
+          this.menuItem = to.name;
+        }
+        //  finish the progress bar
+        this.$Progress.finish();
+      });
+*/
+    },
+    computed: {
+/*
+      ...mapState("user", {
+        user: "user"
+      }),
+      auth () {
+        return auth;
+      },
+      activeMenuItem() {
+        return this.menuItem;
+      }
+*/
+    },
+    methods: {
+/*       clickMenu(item) {
+        this.menuItem = item.title;
+        this.$router.push({
+          name: item.title
+        });
+      }, */
+    },
+    mounted () {
+//      this.$Progress.finish();
+    }
 };
 </script>
