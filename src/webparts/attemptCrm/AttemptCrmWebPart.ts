23d868eb54664062b321c58d9a37051397712263
Scheevel <scheevel@gmail.com>
2020-03-27 13:51:45 -0500

    Initial commit

---
+++ src/webparts/attemptCrm/AttemptCrmWebPart.ts
@@ -0,0 +1,68 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+    BaseClientSideWebPart,
+    IPropertyPaneConfiguration,
+    PropertyPaneTextField
+  } from '@microsoft/sp-webpart-base';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+import * as strings from 'AttemptCrmWebPartStrings';
+
+// Importing Vue.js
+import Vue from 'vue';
+import App from './App.vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+Vue.config.productionTip=false;
+Vue.use(Vuetify);
+const vuetifyOpts = {};
+// Importing Vue.js SFC
+//import AttemptCrmComponent from './components/AttemptCrm.vue';
+
+export interface IAttemptCrmWebPartProps {
+  description: string;
+}
+
+export default class AttemptCrmWebPart extends BaseClientSideWebPart<IAttemptCrmWebPartProps> {
+
+  public render(): void {
+    const id: string = `wp-${this.instanceId}`;
+    this.domElement.innerHTML = `<div id="${id}"></div>`;
+
+    let el = new Vue({
+      el: `#${id}`,
+      vuetify: new Vuetify(vuetifyOpts),
+      render: h => h(App, {
+        props: {
+          description: this.properties.description
+        }
+      })
+    });
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/attemptCrm/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IAttemptCrmWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'AttemptCrmWebPartStrings' {
+  const strings: IAttemptCrmWebPartStrings;
+  export = strings;
+}
