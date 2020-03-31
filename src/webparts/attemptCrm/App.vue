60e11d1a766b1c70a2813596c589a0168ac2398d
Scheevel <scheevel@gmail.com>
2020-03-31 18:34:33 -0500

    success. up to progress bar
    works

--- src/webparts/attemptCrm/App.vue
+++ src/webparts/attemptCrm/App.vue
@@ -11,7 +11,7 @@
         <v-list class="pa-0">
           <v-list-tile avatar tag="div">
             <v-list-tile-avatar>
-              <img src="/assets/img/avatar0.png">
+              <img src="https://github.com/harryho/vue2crm/blob/master/assets/img/avatar0.png">
             </v-list-tile-avatar>
             <v-list-tile-content>
                 <!-- todo -->
@@ -20,9 +20,11 @@
             <v-spacer></v-spacer>
             <v-list-tile-action style="min-width:30px;">
               <v-menu bottom right offset-y origin="bottom right" transition="v-slide-y-transition">
-                <v-btn icon light slot="activator">
-                  <v-icon>more_vert</v-icon>
-                </v-btn>
+                <template v-slot:activator="{ on }">
+                  <v-btn icon light v-on="on">
+                    <v-icon>more_vert</v-icon>
+                  </v-btn>
+                </template>
                 <v-list>
                   <v-list-tile v-for="item in userMenus" :key="item.title" value="true" :to="item.link" router>
                     <v-list-tile-title v-text="item.title"></v-list-tile-title>
@@ -50,7 +52,7 @@
           </v-list-tile>
         </v-list>
       </v-navigation-drawer>
-      <v-toolbar app>
+      <v-app-bar app>
         <v-toolbar-side-icon @click.native.stop="drawer = !drawer" light></v-toolbar-side-icon>
         <v-spacer></v-spacer>
         <div class="text-xs-center pr-3">
@@ -64,13 +66,13 @@
             <v-icon large color="grey">mail</v-icon>
           </v-badge>
         </div>
-        <v-btn light flat  href="https://github.com/harryho/vue2crm" target="_blank">
+        <v-btn light text href="https://github.com/harryho/vue2crm" target="_blank">
                 <svg height="30" class="octicon octicon-mark-github" viewBox="0 0 16 16" version="1.1" width="32" aria-hidden="true">
                     <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
                   </svg>
             <span style="margin-left:0.4rem;">GitHub</span>
           </v-btn>
-      </v-toolbar>
+      </v-app-bar>
       <v-content>
         <v-container fluid >
           <!-- <v-layout>
@@ -159,9 +161,10 @@ export default {
         console.log("loggedIn", loggedIn);
         this.loggedIn = loggedIn;
       };
-/* 
+
       //  [App.vue specific] When App.vue is first loaded start the progress bar
       this.$Progress.start();
+/*
       //  hook the progress bar to start before we move router-view
       this.$router.beforeEach((to, from, next) => {
         //  does the page we want to go to have a meta.progress object
--- src/webparts/attemptCrm/AttemptCrmWebPart.ts
+++ src/webparts/attemptCrm/AttemptCrmWebPart.ts
@@ -16,14 +16,31 @@ import Vuetify from 'vuetify';
 import VueRouter from 'vue-router';
 import 'vuetify/dist/vuetify.min.css';
 import '../../../node_modules/material-design-icons/iconfont/material-icons.css';
-import router from './router/index'
-import store from './store/index'
+import router from './router/index';
+import store from './store/index';
+import VueProgressBar from 'vue-progressbar';
 Vue.config.productionTip=false;
 Vue.use(Vuetify);
 Vue.use(VueRouter);
 const vuetifyOpts = {};
 // Importing Vue.js SFC
 //import AttemptCrmComponent from './components/AttemptCrm.vue';
+
+const options = {
+  color: '#2196f3',
+  failedColor: '#874b4b',
+  thickness: '5px',
+  transition: {
+    speed: '0.1s',
+    opacity: '0.5s',
+    termination: 400
+  },
+  autoRevert: true,
+  location: 'top',
+  inverse: false
+}
+
+Vue.use(VueProgressBar, options);
 
 export interface IAttemptCrmWebPartProps {
   description: string;
