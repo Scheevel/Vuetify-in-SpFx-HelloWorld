f3656add7570241d19eacf7e7a4ec414d4a68246
Scheevel <scheevel@gmail.com>
2020-04-06 17:52:31 -0500

    success. installed @pnp sp & graph
    works

--- src/webparts/attemptCrm/ComponentsAndRoutes.ts
+++ src/webparts/attemptCrm/ComponentsAndRoutes.ts
@@ -1,8 +1,9 @@
 import AboutPage from "./pages/About.vue";
-import DataGridPage from "./pages/DataGrid.vue";
+//import DataGridPage from "./pages/DataGrid.vue";
 import SpFxPage from './pages/AttemptCrm.vue';
+import Grid from "./components/Grid.vue";
 
 export const routes = [
   { path: '/about', name: 'About', icon:'mdi-information', component: AboutPage },
-  { path: '/datagrid', name: 'Data Grid', icon:'mdi-database', component: DataGridPage }
+  { path: '/datagrid', name: 'Grid', icon:'mdi-database', component: Grid }
 ]
--- src/webparts/attemptCrm/components/Grid.vue
+++ src/webparts/attemptCrm/components/Grid.vue
@@ -1,161 +1,52 @@
 <template>
-  <div class="crudgrid">
-    <v-toolbar>
-        <v-toolbar-title>{{ title }}</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-dialog v-model="dialog" max-width="500px" @keydown.esc="close" @keydown.enter="save">
-        <v-btn v-if="canAdd" slot="activator" color="primary" class="mb-2">New Item</v-btn>
-        <v-card>
-            <v-card-title>
-            <span class="headline">{{ formTitle }}</span>
-            </v-card-title>
-
-            <v-card-text>
-            <v-container grid-list-md>
-                <v-layout wrap>
-
-                <v-flex v-for="header in headers" :key="header.value" xs12 sm6 md4>
-                    <v-text-field 
-                                v-model="editedItem[header.value]"
-                                :label="header.text" 
-                                v-validate="header.validation" 
-                                :data-vv-name="header.value"
-                                :error-messages="errors.collect(header.value)">                      
-                    </v-text-field>
-                </v-flex>
-                </v-layout>
-            </v-container>
-            </v-card-text>
-
-            <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
-            <v-btn color="blue darken-1" flat @click="save">Save</v-btn>
-            </v-card-actions>
-        </v-card>
-        </v-dialog>
-    </v-toolbar>
-    <v-data-table :headers="headers" :items="rows">
-
-        <template slot="items" slot-scope="props">
-            <td v-for="header in headers" :key="xkey" :class="header.align == 'left' ? 'text-xs-left' : 'text-xs-right'">
-                {{ props.item[header.value] }}
-            </td>
-            <td class="justify-center layout px-0">
-                <v-icon v-if="canEdit" small class="mr-2" @click="editItem(props.item)">
-                    edit
-                </v-icon>
-                <v-icon v-if="canDelete" small @click="deleteItem(props.item)">
-                    delete
-                </v-icon>
-            </td>
-        </template>
-        <template slot="no-data">
-            <v-btn color="primary" @click="initialize">Reset</v-btn>
-        </template>
-    </v-data-table>
-    <confirm ref="confirm"></confirm>
-  </div>
+      <v-table
+            is-vertical-resize
+            
+            :columns="columns"
+            :table-data="tableData"
+            :show-vertical-border="false"
+            :cell-edit-done="cellEditDone"
+            row-hover-color="#eee"
+            row-click-color="#edf7ff"
+    ></v-table>
 </template>
 
 <script lang=ts>
-import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
-import confirm from './Confirm.vue';
+import Vue from "vue";
+import "vue-easytable/libs/themes-base/index.css";
+import { VTable, VPagination } from "vue-easytable";
+Vue.component(VTable.name, VTable);
+Vue.component(VPagination.name, VPagination);
+export default {
+  name: "DataGrid",
+  data: function() {
+            return {
+                  tableData: [
+                        {"name":"赵伟","tel":"156*****1987","hobby":"钢琴、书法、唱歌","address":"上海市黄浦区金陵东路569号17楼"},
+                        {"name":"李伟","tel":"182*****1538","hobby":"钢琴、书法、唱歌","address":"上海市奉贤区南桥镇立新路12号2楼"},
+                        {"name":"孙伟","tel":"161*****0097","hobby":"钢琴、书法、唱歌","address":"上海市崇明县城桥镇八一路739号"},
+                        {"name":"周伟","tel":"197*****1123","hobby":"钢琴、书法、唱歌","address":"上海市青浦区青浦镇章浜路24号"},
+                        {"name":"吴伟","tel":"183*****6678","hobby":"钢琴、书法、唱歌","address":"上海市松江区乐都西路867-871号"}
+                     ],
+                    columns:  [
+                             {field: 'name', title:'姓名', width: 80, titleAlign: 'center',columnAlign:'center',isEdit:true,
+                              formatter: function (rowData,rowIndex,pagingIndex,field) {
 
-@Component
-export default class Grid extends Vue {
-    name: 'grid'
-    @Prop()
-    title: String
-    @Prop()
-    headers: Array<any>
-    @Prop()
-    rows: Array<any>
-    @Prop()
-    canEdit: Boolean
-    @Prop()
-    canDelete: Boolean
-    @Prop()
-    canAdd: Boolean
-  
-    private dialog: boolean = false //are we showing the edit/new record dialog?
-    private editedIndex: number = -1 //Current record in edit
-    private editedItem: object = { 
-        //Copy of information to be edited in the edit/new dialog
-    }
-    private defaultItem: object = { 
-        //Copy defaults for a new item
-    }
-  
-    get formTitle (): string {
-      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
-    }
+                                   return `<span class='cell-edit-color'>${rowData[field]}</span>`;
+                               },isResize:true},
+                             {field: 'tel', title: '手机号码', width: 150, titleAlign: 'center',columnAlign:'center',isEdit:true,isResize:true},
+                             {field: 'hobby', title: '爱好', width: 150, titleAlign: 'center',columnAlign:'center',isEdit:true,isResize:true},
+                             {field: 'address', title: '地址', width: 280, titleAlign: 'center',columnAlign:'left',isEdit:true,isResize:true}
+                     ]
+            }
+  }
+  ,
+  methods: {
+     cellEditDone(newValue,oldValue,rowIndex,rowData,field){
+        this.tableData[rowIndex][field] = newValue;
+        console.log(newValue);
+     }
 
-    public initialize ( editItem=this.editItem ): void {
-      // Create defaultItem based on header information
-
-      this.headers.forEach(header => {
-          this.defaultItem[header.value] = header.hasOwnProperty('default') ? header.default : ''
-        })
-
-      // Copy header information to standard editedItem
-      this.editItem = (<any>Object).assign(this.editItem, this.defaultItem);
-      console.log('can add:  ' + this.canAdd)
-      console.log('can edit: ' + this.canEdit) 
-    }
-
-    @Watch('dialog')
-    public changeDialog (val): void {
-      val || this.close()
-    }
-
-    public close (): void {
-      this.dialog = false;
-      /*
-      setTimeout(() => {
-        this.editedItem = (<any>Object).assign({}, this.defaultItem);
-        this.editedIndex = -1
-      }, 300)
-      */
-    }
-
-    public created (): void {
-        this.initialize()
-    }
-
-    editItem (item) {
-      console.log('Edit: ' + item)
-      this.editedIndex = this.rows.indexOf(item)
-      console.log('Edit Index: ' + this.editedIndex)
-      this.editedItem = (<any>Object).assign({}, item)
-      console.log('Edited Item: ' + this.editedItem)
-      this.dialog = true
-    }
-
-    async deleteItem (item) {
-      const index = this.rows.indexOf(item)
-      this.$refs.confirm.open('Delete', 'Are you sure?', {color: 'red', textOK: 'Delete'}).then( (confirm) => {
-        console.log(`Received from confirm dialog: ${confirm}`)
-        if (confirm) this.rows.splice(index, 1)
-      })
-    }
-    
-    async save () {  
-      console.log("save() called")
-      //let valid = await this.$validator.validateAll()
-      //console.log("Form valid: " + valid)
-      //if (!valid) 
-        //return
-      console.log("Edited Index: " + this.editedIndex)
-      if (this.editedIndex > -1) {
-        //console.log('Original name: ' + this.rows[this.editedIndex].name + ' ' + this.rows[this.editedIndex].calories)
-        //console.log('Edited name: ' + this.editedItem.name + ' ' + this.editedItem.calories)
-        //this.editedItem.calories = 2323
-        //(<any>Object).assign(this.rows[this.editedIndex], this.editedItem)
-      } else {
-        this.rows.push(this.editedItem)
-      }
-      this.close()
-    }
-}
+  }
+};
 </script>
